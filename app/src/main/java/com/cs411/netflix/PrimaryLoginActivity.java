38a7f37562ac477fdd8757ccf821937e94f92675
Farhan Khan <farhan.khan.phx@gmail.com>
2018-11-27 20:15:48 -0600

    fixed primary login dialog functionality

--- app/src/main/java/com/cs411/netflix/PrimaryLoginActivity.java
+++ app/src/main/java/com/cs411/netflix/PrimaryLoginActivity.java
@@ -192,6 +192,12 @@ public class PrimaryLoginActivity extends AppCompatActivity{
                 dialog.show();
 
             }
+            else{
+                //don't edit preferences, only open dashboard
+                Intent intent = new Intent(PrimaryLoginActivity.this, DashboardActivity.class);
+                intent.putExtra("username", sUser);
+                startActivity(intent);
+            }
 
         }
         else{
